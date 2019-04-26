79cf87724a8bdb05bf1804a99f425f3f0fdc6653
roll1226 <tomo_yasu_4724@icloud.com>
2019-04-26 20:58:35 +0900

    7th commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="about">
     <div class="aboutnac">
-      <div @click="me">Me</div>
-      <div @click="skill">Skill</div>
+      <div @click="me" id="meBtn" v-bind:class="{activeMe: activeMe}">
+        <p>Me</p>
+      </div>
+      <div @click="skill" id="skillBtn" v-bind:class="{activeSkill: activeSkill}">
+        <p>Skill</p>
+      </div>
     </div>
     <div class="me" v-if="changeabout">
       <h1>
@@ -22,15 +26,21 @@
 export default {
   data () {
     return {
-      changeabout: true
+      changeabout: true,
+      activeMe: true,
+      activeSkill: false
     }
   },
   methods: {
     me: function () {
       this.changeabout = true
+      this.activeMe = true
+      this.activeSkill = false
     },
     skill: function () {
       this.changeabout = false
+      this.activeMe = false
+      this.activeSkill = true
     }
   }
 }
@@ -53,12 +63,38 @@ export default {
     font-size: 20px;
   }
   .aboutnac {
-  display: flex;
-  flex-flow: row nowrap;
-  justify-content: center;
-  align-items: center;
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: center;
+    align-items: center;
     position: absolute;
-    top: 40vw;
+    top: 37vw;
+    #meBtn p,
+    #skillBtn p {
+      cursor: pointer;
+    }
+    #meBtn p {
+      transition: all 1s ease 0s;
+      padding: 20px;
+      border-radius: 20px;
+      &:hover {
+        box-shadow: 0 0 30px rgba(0,0,0,0.2);
+      }
+    }
+    .activeMe {
+      color: #0af;
+    }
+    #skillBtn p {
+      transition: all 1s ease 0s;
+      padding: 20px;
+      border-radius: 20px;
+      &:hover {
+        box-shadow: 0 0 30px rgba(0,0,0,0.2);
+      }
+    }
+    .activeSkill {
+      color: #0af;
+    }
   }
 }
 </style>
